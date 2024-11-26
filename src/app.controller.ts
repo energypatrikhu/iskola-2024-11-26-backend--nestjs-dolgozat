7f7cb616ef6a52c9a9372db612581baffbf14e00
EnergyPatrikHU <energypatrikhu@proton.me>
2024-11-26 14:43:12 +0100

    Implement CRUD operations for travels in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,87 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Render,
+  Req,
+  Search,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Travels } from './travels';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index')
-  getHello() {
-    return {
-      message: this.appService.getHello()
-    };
+  private travels = new Travels();
+
+  @Get('/travels')
+  getTravels() {
+    return this.travels.getAll();
+  }
+
+  @Get('/travels/:id')
+  getTravelById(@Param('id') id: string) {
+    return this.travels.getById(parseInt(id));
+  }
+
+  @Post('/travels')
+  addTravel(
+    @Body()
+    data: {
+      destination: string;
+      description: string;
+      imgUrl: string;
+      price: number;
+    },
+  ) {
+    if (!data.price || !data.destination || !data.description || !data.imgUrl) {
+      return 'Hiányzó adatok!';
+    }
+
+    return this.travels.add({
+      destination: data.destination,
+      description: data.description,
+      imgUrl: data.imgUrl,
+      price: data.price,
+      discount: 0,
+    });
+  }
+
+  @Patch('/travels/:id')
+  updateTravel(
+    @Param('id') id: string,
+    @Body()
+    data: {
+      destination?: string;
+      description?: string;
+      imgUrl?: string;
+      price?: number;
+      discount?: number;
+    },
+  ) {
+    const travel = this.travels.getById(parseInt(id));
+    if (!travel) {
+      return 'Utazás nem található!';
+    }
+
+    let newData = { ...travel };
+
+    newData.destination = data.destination || travel.destination;
+    newData.description = data.description || travel.description;
+    newData.imgUrl = data.imgUrl || travel.imgUrl;
+    newData.price = data.price || travel.price;
+    newData.discount = data.discount || travel.discount;
+
+    this.travels.replace(parseInt(id), newData);
+  }
+
+  @Delete('/travels/:id')
+  deleteTravel(@Param('id') id: string) {
+    this.travels.remove(parseInt(id));
   }
 }
